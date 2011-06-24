68d9ed2b67c60751db88b1f6656e886dd0a2c496
jinchizhong <jinchizhong@bf1e31fd-e268-4f19-b032-1b0deef43461>
2011-06-24 13:16:46 +0000

    解决控件只接收到enter事件不接收到leave事件的问题。
    git-svn-id: http://svn.wps.kingsoft.net/wpsenv/trunk/3rdparty/qt@27 bf1e31fd-e268-4f19-b032-1b0deef43461

--- src/gui/kernel/qapplication_win.cpp
+++ src/gui/kernel/qapplication_win.cpp
@@ -2924,9 +2924,32 @@ static const ushort mouseTbl[] = {
 
 static int translateButtonState(int s, int type, int button)
 {
-    Q_UNUSED(type);
+    // modifyed by kingsoft
+    // Windows nc message's wParam is hit test result instead of flags
     Q_UNUSED(button);
     int bst = 0;
+
+    if (type == QEvent::NonClientAreaMouseMove || type == QEvent::NonClientAreaMouseButtonPress
+        || type == QEvent::NonClientAreaMouseButtonRelease || type == QEvent::NonClientAreaMouseButtonDblClick)
+    {
+        if (GetKeyState(VK_LBUTTON) < 0)
+            bst |= Qt::LeftButton;
+        if (GetKeyState(VK_MBUTTON) < 0)
+            bst |= Qt::MidButton;
+        if (GetKeyState(VK_RBUTTON) < 0)
+            bst |= Qt::RightButton;
+        if (GetKeyState(VK_SHIFT) < 0)
+            bst |= Qt::ShiftModifier;
+        if (GetKeyState(VK_CONTROL))
+            bst |= Qt::ControlModifier;
+
+        if (GetKeyState(VK_XBUTTON1))
+            bst |= Qt::XButton1;
+        if (GetKeyState(VK_XBUTTON2))
+            bst |= Qt::XButton2;
+    }
+    else
+    {
     if (s & MK_LBUTTON)
         bst |= Qt::LeftButton;
     if (s & MK_MBUTTON)
@@ -2942,7 +2965,7 @@ static int translateButtonState(int s, int type, int button)
         bst |= Qt::XButton1;
     if (s & MK_XBUTTON2)
         bst |= Qt::XButton2;
-
+    }
     if (GetKeyState(VK_MENU) < 0)
         bst |= Qt::AltModifier;
 
